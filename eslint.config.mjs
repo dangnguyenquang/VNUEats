62fa422ef3a4e4b26333e0c5423b2f9c5a2468cd
NguyenQuangDang <dangnguyenquangit@gmail.com>
2025-02-25 18:05:15 +0700

    feat: init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/display-name": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
