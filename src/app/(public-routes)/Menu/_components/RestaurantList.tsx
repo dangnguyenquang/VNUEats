682db1f5acee7f0dea9a4ffbcf80b8bd307f5d7c
NguyenQuangDang <dangnguyenquangit@gmail.com>
2025-03-25 09:55:02 +0700

    fix: rename folder

--- src/app/(public-routes)/Menu/_components/RestaurantList.tsx
+++ src/app/(public-routes)/Menu/_components/RestaurantList.tsx
@@ -17,6 +17,8 @@ type RestaurantListProps = {
   isFilterOpen: boolean
 }
 
+//
+
 const RestaurantList: React.FC<RestaurantListProps> = ({ Restaurants, isFilterOpen }) => {
   return isFilterOpen ? (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5">
