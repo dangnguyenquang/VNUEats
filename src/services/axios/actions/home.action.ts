f0e5d83c6d0dfeb9c4c9a96e50bab07018d52d18
NguyenQuangDang <dangnguyenquangit@gmail.com>
2025-04-02 11:51:12 +0700

    feat: update ssr in home

--- src/services/axios/actions/home.action.ts
+++ src/services/axios/actions/home.action.ts
@@ -1,18 +1,15 @@
-import { client } from "@/services/axios"
+"use server"
+
+import { server } from "@/services/axios"
 import { FoodData } from "@/types/api/home"
 import homeEndpoint from "../endpoints/home.endpoint"
 
-class HomeApi {
-    async getHome(): Promise<FoodData> {
-        try {
-            const res = await client.get(homeEndpoint.gethome)
-            return res.data
-        } catch (error) {
-            throw new Error("Đã có lỗi xảy ra")
-        }
+export async function getHome(): Promise<FoodData> {
+    try {
+        const res = await server(homeEndpoint.gethome)
+
+        return res
+    } catch (error) {
+        throw new Error("Đã có lỗi xảy ra")
     }
 }
-
-const homeApiInstance = new HomeApi()
-
-export default homeApiInstance
